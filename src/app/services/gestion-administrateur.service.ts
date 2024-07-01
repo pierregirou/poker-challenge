1b8d4579df012ad1f03fc60ae0d39dcf10ce0879
pierregirou <pierre.girou42290@gmail.com>
2024-07-01 09:30:01 +0200

    ajout text challenge privé/public/perso/multi

--- src/app/services/gestion-administrateur.service.ts
+++ src/app/services/gestion-administrateur.service.ts
@@ -1,12 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GestionAdministrateurService {
-  constructor(private http:HttpClient,private userService:UserService) { }
+  constructor(private http:HttpClient) { }
  
   signin(mail:string,password:string){
     this.http.post('https://pg-dev.fr/classement_mtt_winamax/connexion.php',{
@@ -14,12 +13,9 @@ export class GestionAdministrateurService {
       'mail':mail,
       'password':password
     }).subscribe({
-      next: (value:any)=>{
-        console.log(value)
-        this.userService.user = value['response']
-      },
-      error:(error)=>{console.error},
-      complete:()=>console.log('complete http request signIn')
+      next: ((value)=>{console.log(value)}),
+      error: ((error)=>console.error),
+      complete:(()=>console.log('complete http request signIn'))
     })
   }
 
